b684d2600e90fbba9c34b2cae25125d4e15ee3a9
Preciousglucode <precious@glucode.com>
2023-04-11 08:48:09 +0200

    collection and search
    collection v

---
+++ Loadshedding/Loadshedding/UICollectionViewController..swift
@@ -0,0 +1,12 @@
+//
+//  UICollectionViewController..swift
+//  Loadshedding
+//
+//  Created by Precious Omoroga on 2023/04/10.
+//
+
+import UIKit
+
+class UICollectionViewController_: LoadSheddingViewController. {
+
+}
